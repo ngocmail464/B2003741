d7b51a1c5100252cf9097e72fd52da069350104c
Ngọc Hương <126375191+ngocmail464@users.noreply.github.com>
2023-03-14 12:45:55 +0700

    Add files via upload

---
+++ formnhap.php
@@ -0,0 +1,26 @@
+<!DOCTYPE HTML>
+<html>  
+<body>
+<label for="cars">Choose a car:</label>
+
+<select id="cars">
+  <option value="1">1</option>
+  <option value="2">2</option>
+  <option value="3">3</option>
+  <option value="4">4</option>
+  <option value="5">5</option>
+  <option value="6">6</option>
+  <option value="7">7</option>
+  <option value="8">8</option>
+  <option value="9">9</option>
+</select>
+<form action="luu.php" method="post">
+Name: <input type="text" name="name"><br>
+E-mail: <input type="text" name="email"><br>
+Birthday: <input type="date" name="birth"><br>
+
+<input type="submit"> 
+</form>
+
+</body>
+</html>
